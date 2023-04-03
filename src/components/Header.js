8a7f88340deaabaf85731d715647fb1827bf1d63
[Srikant Agrawal] <[srikant9004@gmail.com]>
2023-04-03 19:28:21 +0530

    modified:   src/components/Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,28 @@ import logo from '../seme-logo.png'
 
 function Navbar() {
     return (
-        <nav className="pl-1 navbar navbar-expand-lg text-xl font-mono border-b-slate-200 border-b-2 h-[10vh]" style={{backgroundColor: "#61C0BF"}}>
-            <a className="navbar-brand flex align-center">
+        <nav className="pl-1 flex text-xl font-mono border-b-slate-200 border-b-2 h-[10vh] align-center " style={{backgroundColor: "#61C0BF"}}>
+            <a className="basis-1/2 flex align-center">
                 <Link to="/" className="nav-link bg-white">
-                <img className="h-[8vh] w-full" src={logo} />
+                <img className="h-[8vh] w-auto" src={logo} />
                 </Link>
             </a>
-            <a className="nav-items ms-auto mr-10">
+            <a className='basis-1/2 flex justify-end'>
+            <a className="ms-auto p-3">
                 <Link className="nav-link" to="/entry"> 
                     Entry
                 </Link>
             </a>
-            <a className="nav-items mr-10">
+            <a className="p-3">
                 <Link className="nav-link" to="/exit">
                     Exit
                 </Link>
             </a>
-            <a className="nav-items mr-10">
+            <a className="p-3">
                 <Link className="nav-link" to="/check">
                     Check
                 </Link>
+            </a>
             </a>
             {/* <a className="nav-items mr-10">
                 <Link className="nav-link" to="/credits">
