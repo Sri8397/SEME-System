0464fbe78757182580bfce3c9e7eb29d1839acca
sri8397 <[srikant9004@gmail.com]>
2023-04-06 17:20:11 +0530

    modified:   src/components/Check.js

--- src/components/Check.js
+++ src/components/Check.js
@@ -2,7 +2,7 @@
 // import { useState } from 'react'
 
 const Check = () => {
-  localStorage.removeItem('entry')
+  // localStorage.removeItem('entry')
   let data2 = JSON.parse(localStorage.getItem('tehkikat'))
   let data1 = JSON.parse(localStorage.getItem('entry'))
 
