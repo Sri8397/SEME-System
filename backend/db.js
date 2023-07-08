f17fe805599a8be78ea85b49d27a44d4b546f4ff
sri8397 <[srikant9004@gmail.com]>
2023-07-09 02:16:33 +0530

    ..

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); 
+require('dotenv').config();
 
+mongoose.set("strictQuery", false);
 
 const connectToMongo = () => {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() =>
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() =>
         console.log(`Connected to mongodb sucessfully`)
-    );
+    )
+    .catch((err) => console.log(err));
 }
 module.exports = connectToMongo; 
