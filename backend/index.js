63e4e9f808aa78b2a7aa5338462a5676645be074
sri8397 <[srikant9004@gmail.com]>
2023-07-09 12:15:36 +0530

    modified:   backend/index.js 	modified:   backend/package.json

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 connectToMongo();
 //serving frontend using middlewares
